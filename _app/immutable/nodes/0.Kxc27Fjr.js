e0f2b83edcfe1f121dacfc12156df8bb407bb69b
Yuri Goytacaz <yurigoytacaz@gmail.com>
2026-07-15 11:28:17 -0300

    Updates

---
+++ _app/immutable/nodes/0.Kxc27Fjr.js
@@ -0,0 +1,1 @@
+import{F as e,J as t,p as n,v as r,y as i}from"../chunks/aPyKf_3b.js";import"../chunks/xihTtKlq.js";var a=t({prerender:()=>!0,ssr:()=>!1});function o(t,a){var o=i();n(e(o),()=>a.children),r(t,o)}export{o as component,a as universal};
